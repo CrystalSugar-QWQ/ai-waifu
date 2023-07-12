d686804ffc4742bde43a623628c8c95ac210e413
CrystalSugar <68407950+CrystalSugar-QWQ@users.noreply.github.com>
2023-07-12 12:24:01 +0800

    Add files via upload

---
+++ new akato/README.md
@@ -0,0 +1,23 @@
+# ai-waifu
+本项目主旨是做一个ai-waifu,能灵活自由聊天的ai,[效果视频](https://www.bilibili.com/video/BV1Qa4y1F7qN/?share_source=copy_web&vd_source=be871ab215f9686e9cf85ae02546df3f)  
+由于作者刚学习python一个月,能力有限见谅  
+想更改设定于openao/identity.txt里更改
+有问题或技术支持请联系+Q：1262852040  
+python版本3.10  
+## 使用的外部软件
+1、下载Vtube studio软件，于steam上下载即可  
+2、[下载voicevox语音合成软件](https://voicevox.hiroshiba.jp/)  
+3、[下载虚拟音频线软件](https://vb-audio.com/Cable/index.htm),安装时请使用管理员权限打开  
+## 如何使用
+1、安装python库  
+`pip install -r requirements.txt`  
+2、打开Vtube studio,找到启用API选项并打开,麦克风配置里打开使用麦克风,选择麦克风为**CABLE Output**,  
+   动作输入将Mouth Smile的输入改为**VoiceVolume**,将Mouth Open的输入改为**VoiceFrequency**  
+3、打开voicevox,注意不是打开VOICEVOX.exe,而是**run.exe**  
+4、找到config.py文件，填入你[openai API](https://platform.openai.com/account/api-keys)  
+5、启动前打开音量合成器,将程序的输入改成**你的麦克风**,输出改为**CABLE Input**  
+再打开系统的更多声音设置,再录制里找到**CABLE Output**右键点击属性,找到侦听勾选**侦听此设备**,播放设备选择你的默认扬声器（你不做这步是听不见语音的）  
+6、启动后若频繁触发语音录入,请进入VoiceRW/tts.py中,找到`audio_listien`函数中的音量阈值参数,调高它,反之无法触发调小它  
+7、B站直播间弹幕获取也是有的,进入blivedm/sample.py,在`TEST_ROOM_IDS`填入直播间id,  
+再找到`class MyHandler(blivedm.BaseHandler)`函数，`_on_danmaku`里面有触发问答的方式,比如弹幕里提到**白糖**,AI就会回复  
+`if "白糖" in message.msg:`触发是这一段捏,可以多改改设定成只回复特定话题  
---
+++ new akato/run.py
@@ -0,0 +1,220 @@
+import sys
+import subtitle
+import random
+import open_ai.akato_ai
+import Translate.MyTranslation
+import VoiceRW.tts
+import vts.vtube
+import multiprocessing
+import blivedm.sample
+import asyncio
+from VoiceRW.tts import Voicevox
+import speech_recognition as sr
+import pyaudio, wave
+from aip import AipSpeech
+
+sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
+
+owner_name = "sugar"
+audio_input = "input.wav"
+
+
+# 异步处理问题并输出
+def ask_out(question, message_data, question2, tts_data):
+    while True:
+        # 从问题队列获取问题
+        text = question.get()
+
+        # 想ai提问，获取回答
+        message = open_ai.akato_ai.ask_ai(owner_name, text)
+        print("[Akato]:", message)
+
+        # voicevox的输入翻译
+        # tts = Translate.MyTranslation.translate_GPT3(message, "Japanese")
+        tts = Translate.MyTranslation.translate_google(message, "JA")
+        print("[翻译器][日语]:", tts)
+
+        question2.put(text)
+        message_data.put(message)
+        tts_data.put(tts)
+
+
+# 语音阅读+动作触发
+def voice_out(question, action_data, message_data, tts_data):
+    vv = Voicevox()
+    while True:
+        # 从问题队列获取问题
+        text = question.get()
+        message = message_data.get()
+        tts = tts_data.get()
+
+        try:
+            # 生成语音文件
+            #VoiceRW.tts.voicevox_tts(tts, 46)
+
+            # 生成字幕文本
+            subtitle.generate_subtitle(text, message)
+
+            # 阅读语音
+            #VoiceRW.tts.read_wav()
+
+            # 生成+阅读语音
+            vv.speak(text = tts, speaker = 46)
+        except Exception:
+            print("[VoiceVox ERROR]")
+        finally:
+            # 概率性触发动作
+            sp = random.randint(1,100)
+            if sp > 0 and sp <= 2:
+                action_data.value = 4
+            elif sp > 2 and sp <= 10:
+                action_data.value = 3
+            elif sp > 10 and sp <= 40:
+                action_data.value = 1
+            elif sp > 40 and sp <= 100:
+                action_data.value = 2
+
+            # 清空字幕文本
+            with open("output.txt", "w") as f:
+                f.truncate(0)
+            with open("chat.txt", "w") as f:
+                f.truncate(0)
+
+
+def vtube_sport(shared_data):
+    vts.vtube.vtube_worker(shared_data)
+
+
+def BliveDM(question):
+    asyncio.run(blivedm.sample.main(question))
+
+
+'''
+def audio_to_text(question, audio_data):
+    while True:
+        r = sr.Recognizer()
+        audio = audio_data.get()
+        try:
+            text = r.recognize_google(audio, language='zh-CN')
+            # 输出识别结果
+            print("[question]:" + text)
+            question.put(text)
+        except Exception:
+            print("[语音识别异常]")
+
+
+def listen_google():
+    audio = VoiceRW.tts.audio_listen_google()
+    return audio
+'''
+
+
+# 语音(wav)翻译——谷歌
+def wave_to_text_google(file):
+    r = sr.Recognizer()
+    with sr.AudioFile(file) as source:
+        audio = r.record(source)
+    try:
+        text = r.recognize_google(audio, language='zh-CN')
+        return text
+    except sr.UnknownValueError:
+        print("[无法识别输入的语音]")
+    except sr.RequestError as e:
+        print("[请求出错]:" + str(e))
+
+
+# 语音(wav)翻译——百度
+def wave_to_text_baidu(file):
+    # 设置 APPID、API Key 和 Secret Key 申请地址https://console.bce.baidu.com/ai/#/ai/speech/overview/index
+    APP_ID = ''
+    API_KEY = ''
+    SECRET_KEY = ''
+    # 初始化 AipSpeech 对象
+    client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+
+    # 读取音频文件
+    with open(file, 'rb') as fp:
+        audio = fp.read()
+
+    # 识别音频文件
+    res = client.asr(audio, 'wav', 16000, {
+        'dev_pid': 1536,
+    })
+    if res['err_no'] == 0:
+        return res['result'][0]
+    else:
+        print("[ERROR baidu:]",res['err_no'])
+
+
+# question是我的提问队列，不用可以改掉
+def wave_to_text(question, audio_data):
+    # 设置音频参数
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 16000
+    WAVE_OUTPUT_FILENAME = "input.wav"  # 保存路径自己改吧，摆烂了
+
+    while True:
+        frames = audio_data.get()
+
+        # 将音频保存为WAV文件
+        with wave.open(WAVE_OUTPUT_FILENAME, 'wb') as wf:
+            wf.setnchannels(CHANNELS)
+            wf.setsampwidth(pyaudio.get_sample_size(FORMAT))
+            wf.setframerate(RATE)
+            wf.writeframes(b''.join(frames))
+
+        # OPENAI翻译
+        # open_ai.akato_ai.transcribe_audio(WAVE_OUTPUT_FILENAME)
+        # 谷歌翻译
+        # wave_to_text_google(WAVE_OUTPUT_FILENAME)
+        # 百度翻译
+        text = wave_to_text_baidu(WAVE_OUTPUT_FILENAME)
+        
+        try:
+            # 输出识别结果
+            print("[question]:" + text)
+            
+            # 在这里输出结果
+            question.put(text)
+        except Exception:
+            pass
+
+# 有封装语音时无法再次录入的bug
+def listen_wave():
+    audio = VoiceRW.tts.audio_listen()
+    return audio
+
+
+if __name__ == '__main__':
+    # 创建队列
+    question = multiprocessing.Queue(maxsize=0)
+    question2 = multiprocessing.Queue(maxsize=0)
+    message = multiprocessing.Queue(maxsize=0)
+    tts_data = multiprocessing.Queue(maxsize=0)
+    audio_data = multiprocessing.Queue(maxsize=0)
+    action_data = multiprocessing.Value('i', 0)
+
+    # 创建进程并启动 ps:别问我为什么只开进程，懒的
+    p1 = multiprocessing.Process(target=ask_out, args=(question, message, question2, tts_data))
+    # p1.start()
+    p2 = multiprocessing.Process(target=vtube_sport, args=(action_data,))
+    p2.start()
+    p3 = multiprocessing.Process(target=BliveDM, args=(question, ))
+    p3.start()
+    p4 = multiprocessing.Process(target=voice_out, args=(question2, action_data, message, tts_data))
+    p4.start()
+    p5 = multiprocessing.Process(target=wave_to_text, args=(question, audio_data))
+    p5.start()
+
+    while True:
+        q = listen_wave()
+        # 向问题队列放入问题
+        audio_data.put(q)
+
+    # 主进程等待子进程结束
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+    p5.join()
