cfd7b852fd535ffc63a6bf09e0d2aefea27d8bd8
CrystalSugar <68407950+CrystalSugar-QWQ@users.noreply.github.com>
2023-07-13 17:12:08 +0800

    Update setup.py

--- new akato/vts/setup.py
+++ new akato/vts/setup.py
@@ -1,25 +1,28 @@
-import vts.uutils as uutils
+import utils as utils
 import json
 
+file_json = "config.json"   # 秘钥保存文件位置
 
 async def init(websocket):
-    with open("vts/config.json", "r") as config_file:     # 读文件
+    with open(file_json, "r") as config_file:     # 读文件
         data = json.load(config_file)
         if data['authenticationkey'] == "":     # 判断是否没有令牌
-            authtoken = await uutils.vtube_token(websocket)
+            authtoken = await utils.vtube_token(websocket)
+            confirm = await utils.vtube_plugin(websocket, authtoken)
             print("[VTS API KEY]:", authtoken)
             data["authenticationkey"] = authtoken
         else:
             authtoken = data['authenticationkey']
-            confirm = await uutils.vtube_plugin(websocket, authtoken)
+            confirm = await utils.vtube_plugin(websocket, authtoken)
             if confirm is False:
-                authtoken = await uutils.vtube_token(websocket)
+                authtoken = await utils.vtube_token(websocket)
+                confirm = await utils.vtube_plugin(websocket, authtoken)
                 data["authenticationkey"] = authtoken
             print("[VTS API KEY]:", authtoken)
-    config_file.close()
+        config_file.close()
     config = {
         "authenticationkey": data["authenticationkey"],
     }
-    config_file = open("vts/config.json", "w")
+    config_file = open(file_json, "w")
     config_file.write(json.dumps(config))
     config_file.close()
