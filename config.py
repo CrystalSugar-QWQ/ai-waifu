adfc97491a77449996278a0144274c9f2c1c7368
CrystalSugar <68407950+CrystalSugar-QWQ@users.noreply.github.com>
2023-07-30 23:01:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@
 # 实际上因为这些东西完全不会变化直接开摆
 
 # 百度智能云api
-wav2text_baidu_APP_ID = '36050058'
-wav2text_baidu_API_KEY = 'xdENMcNpGiQmeNPEDlPuF46Y'
-wav2text_baidu_SECRET_KEY = 'W5Aw2BrM78XEatjwNXiC3h4itEHlwTjM'
+wav2text_baidu_APP_ID = ''
+wav2text_baidu_API_KEY = ''
+wav2text_baidu_SECRET_KEY = ''
 
 # 百度翻译开放平台api
-translate_baidu_appid = '20230707001737106'
-translate_secretKey = 'gXBBiujfAu7wGV57d7Um'
-translate_url_baidu = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
+translate_baidu_appid = ''
+translate_secretKey = ''
+translate_url_baidu = ''
 
 # openai api
-api_key = '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'
+api_key = ''
